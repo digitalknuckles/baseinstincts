4d69b86ed193716fde96b95aeecf2742090a89b9
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-13 14:37:28 -0400

    Update OpenSea API endpoint for fetching NFTs

--- api/opensea.js
+++ api/opensea.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
   const { address } = req.query;
 
-  const r = await fetch(
-    `https://api.opensea.io/api/v2/chain/ethereum/account/${address}/nfts?collection=0x40941ee94980e0fc623311b31c20fbae56673d81`,
-    {
-      headers: {
-        "X-API-KEY": process.env.OPENSEA_API
-      }
+const r = await fetch(
+  `https://api.opensea.io/api/v2/chain/ethereum/contract/0x40941ee94980e0fc623311b31c20fbae56673d81/nfts?limit=50`,
+  {
+    headers: {
+      "X-API-KEY": process.env.OPENSEA_API
     }
-  );
+  }
+);
 
   const data = await r.json();
 
