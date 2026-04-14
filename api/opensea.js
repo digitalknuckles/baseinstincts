5317bcb672ee88695aa242a184837b47199c71a1
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-13 21:15:43 -0400

    Fix response format in OpenSea API handler

--- api/opensea.js
+++ api/opensea.js
@@ -30,8 +30,9 @@ export default async function handler(req, res) {
       }))
       .filter(n => n.image);
 
-   // return res.status(200).json({ nfts });
-    return {
+   return res.status(200).json({ nfts });
+    
+   return {
   image: image_url,
   name: name,
   attributes: metadata.attributes // 🔥 REQUIRED
